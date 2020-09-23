ede208905f04c26490b073378b8706ccbf54daf1
Matthew Anderson <anderson.c.matt@gmail.com>
2020-09-22 22:32:13 -0500

    Added more dependencies to index.js

--- src/assets/common.blocks/index.js
+++ src/assets/common.blocks/index.js
@@ -4,3 +4,10 @@ require('./body/body.css')
 
 require('./header/header.css')
 require('./header/heading/heading.css')
+require('./header/nav/nav.css')
+require('./header/nav/nav__arrow.css')
+require('./header/nav/nav__text.css')
+require('./header/nav/nav_dropdown.css')
+require('./header/nav/nav_hover.css')
+
+require('./modifiers/nav_center.css')
