fbfba5269140e70e9cac1289035486b8042c855a
Matthew Anderson <anderson.c.matt@gmail.com>
2020-10-19 15:55:22 -0500

    Rearranged dependencies for index bundle

--- src/assets/index.blocks/index.js
+++ src/assets/index.blocks/index.js
@@ -1,29 +1,30 @@
 // JS
 
 require('./main/glide/glide')
-require('./asideRight/calendar/calendar')
+require('./asideLeft/calendar/calendar')
 
 // CSS
 
+// Intro
 require('./intro/intro.css')
 require('./intro/intro__break.css')
 require('./intro/intro__text.css')
 
+// Aside
 require('./asideLeft/asideLeft.css')
+require('./asideLeft/calendar/calendar.css')
 
+// Main
 require('./main/main.css')
+require('@glidejs/glide/dist/css/glide.core.css')
+require('@glidejs/glide/dist/css/glide.theme.min.css')
+require('./main/glide/glide__image.css')
+require('./main/glide/glide__slides.css')
 
-
-
-require('./asideRight/asideRight.css')
-require('./asideRight/calendar/calendar.css')
-
+// Images
 require('../../img/IMG-0001.jpg')
 require('../../img/IMG-0002.jpg')
 require('../../img/IMG-0003.jpg')
 require('../../img/IMG-0004.jpg')
 
-require('@glidejs/glide/dist/css/glide.core.css')
-require('@glidejs/glide/dist/css/glide.theme.min.css')
-require('./main/glide/glide__image.css')
-require('./main/glide/glide__slides.css')
+
