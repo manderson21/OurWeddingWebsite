947ee231e6dc914f1c7746b3a47242a1f7f7b0d4
Matthew Anderson <anderson.c.matt@gmail.com>
2020-11-13 14:34:12 -0600

    index.js for weddingParty.html

---
+++ src/assets/weddingParty.blocks/index.js
@@ -0,0 +1,23 @@
+// JS
+require('./main/glide/glide')
+
+
+// Main
+require('./main/main.css')
+require('./main/main__header.css')
+require('./main/parents/main__parents.css')
+require('./main/parents/bridesParents.css')
+require('./main/parents/groomsParents.css')
+require('./main/parents/parents__img.css')
+require('./main/parents/parents__label.css')
+
+// Glide
+require('@glidejs/glide/dist/css/glide.core.css')
+require('@glidejs/glide/dist/css/glide.theme.min.css')
+require('./main/glide/glide.css')
+require('./main/glide/glide__slides.css')
+require('./main/glide/glide__arrow.css')
+require('./main/glide/glide__image.css')
+require('./main/glide/glide__label.css')
+require('./main/glide/glide__bullet.css')
+require('./main/glide/glide__bullets.css')
